fa42bb311e393cb2952c3da936a613568f5449a4
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-09-06 21:27:55 +0530

    update(item) on clicking related product, scroll to the top

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -5,7 +5,7 @@ const Item = (props) => {
   return (
     <div className={styles.item}>
       <Link to={`/product/${props.id}`}>
-        <img src={props.image} alt="" />
+        <img src={props.image} onClick={window.scrollTo(0, 0)} alt="" />
       </Link>
 
       <p>{props.name}</p>
