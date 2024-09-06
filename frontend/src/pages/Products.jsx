66d57ac9a1738446c5ea82a7b15376b6cdc3a1ce
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-09-06 21:25:26 +0530

    feat: add related products  in product

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import Breadcrums from "../components/Breadcrums/Breadcrums";
 import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../components/RelatedProducts/RelatedProducts";
 
 export const Products = () => {
   const { all_products } = useContext(ShopContext);
@@ -15,6 +16,7 @@ export const Products = () => {
       <Breadcrums product={product} />
       <ProductDisplay product={product} />
       <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 };
